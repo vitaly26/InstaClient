50cc3f22d195add97a7927d1fe75c210961de6e3
Vitaly Pastushin <vitaliy2688@yandex.ru>
2013-07-23 17:20:20 +0400

    Init.

---
+++ InstaClient/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  InstaClient
+//
+//  Created by Vitaly on 23.07.13.
+//  Copyright (c) 2013 Vitaly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ InstaClient/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  InstaClient
+//
+//  Created by Vitaly on 23.07.13.
+//  Copyright (c) 2013 Vitaly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
